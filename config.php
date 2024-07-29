bc6fed3cdb55d80376d3af6de396de799f67baed
Rushikesh Dhondge <65481750+RushikeshD41@users.noreply.github.com>
2024-07-29 18:48:06 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,10 @@ $username = "studentUser";
 $password = "password";
 $dbname = "studentDB";
 
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
